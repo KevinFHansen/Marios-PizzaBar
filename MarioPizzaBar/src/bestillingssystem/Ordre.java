8a2baf3eaaa87fa95a759b7a0f7695cb27bbc545
jmunkc <jona04g8@stud.kea.dk>
2021-04-13 12:46:00 +0200

    ordre update

--- MarioPizzaBar/src/bestillingssystem/Ordre.java
+++ MarioPizzaBar/src/bestillingssystem/Ordre.java
@@ -3,29 +3,74 @@ package bestillingssystem;
 import java.util.ArrayList;
 import Menukort_Pizza.Pizza;
 import java.util.Scanner;
+import java.time.LocalTime;
+
+import static Menukort_Pizza.Menukort.pizzaListe;
 
 public class Ordre {
     Scanner sc = new Scanner(System.in);
 
     // Atributter
     private int ordreId;
-    private int bestillingstidspunkt;
+    private LocalTime bestillingstidspunkt;
     private int samletProduktionstid;
     private int afhentningstidspunkt;
     private ArrayList<Pizza> ordreListe = new ArrayList<Pizza>();
 
     // Constructor
-    public Ordre(int ordreId, int bestillingstidspunkt, int samletProduktionstid,
+    public Ordre(int ordreId, int samletProduktionstid,
                  int afhentningstidspunkt, ArrayList<Pizza> ordreListe){
         this.ordreId = ordreId;
-        this.bestillingstidspunkt = bestillingstidspunkt;
+        this.bestillingstidspunkt = bestillingstidspunkt.now();
         this.samletProduktionstid = samletProduktionstid;
         this.afhentningstidspunkt = afhentningstidspunkt;
         this.ordreListe = ordreListe;
 
     }
 
+    // Getters
+    public int getOrdreId(){
+        return this.ordreId;
+    }
+
+    public int getBestillingstidspunkt(){
+        return this.bestillingstidspunkt;
+    }
+
+    public int getSamletProduktionstid(){
+        return this.samletProduktionstid;
+    }
+
+    public int getAfhentningstidspunkt(){
+        return this.afhentningstidspunkt;
+    }
+
+    public ArrayList<Pizza> getOrdreListe(){
+        return this.ordreListe;
+    }
+
+    // Setters
+    public void setOrdreId(int ordreId){
+        this.ordreId = ordreId;
+    }
+
+    public void setBestillingstidspunkt(int bestillingstidspunkt){
+        this.bestillingstidspunkt = bestillingstidspunkt;
+    }
+
+    public void setSamletProduktionstid(int samletProduktionstid){
+        this.samletProduktionstid = samletProduktionstid;
+    }
+
+    public void setAfhentningstidspunkt(int afhentningstidspunkt){
+        this.afhentningstidspunkt = afhentningstidspunkt;
+    }
+
+
     public void opretOrdre(){
+
+        System.out.println(pizzaListe);
+
         Ordre nyOrdre = new Ordre();
 
     }
