a87f7cdc56086999ff69a8c36b11000044cf872e
MichaelDyvad <michaeldyvad@gmail.com>
2021-04-13 10:28:07 +0200

    Menukort

--- MarioPizzaBar/src/Menukort.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class Menukort {
-    String dato;
-    ArrayList<Pizza> pizzaListe;
-
-
-}
---
+++ MarioPizzaBar/src/Menukort_Pizza/Menukort.java
@@ -0,0 +1,25 @@
+package Menukort_Pizza;
+
+import java.util.ArrayList;
+
+public class Menukort {
+    private String dato;
+    private ArrayList<Pizza> pizzaListe = new ArrayList<Pizza>();
+
+    public ArrayList<Pizza> getPizzaListe(){
+        return this.pizzaListe;
+    }
+
+
+
+    public void visPizzaListe(){
+        System.out.println(getPizzaListe());
+    }
+
+    public int fjernPizza(int fjern){
+    getPizzaListe().remove(fjern);
+    return fjern;
+    }
+
+
+}
