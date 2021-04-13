8bc2a5d1c5e93ce163dad63a13dc957023e5cf45
jmunkc <jona04g8@stud.kea.dk>
2021-04-13 10:35:06 +0200

    update

--- MarioPizzaBar/src/bestillingssystem/Ordre.java
+++ MarioPizzaBar/src/bestillingssystem/Ordre.java
@@ -1,6 +1,25 @@
 package bestillingssystem;
 
+import java.util.ArrayList;
+
 public class Ordre {
+    // Atributter
+    private int ordreId;
+    private int bestillingstidspunkt;
+    private int samletProduktionstid;
+    private int afhentningstidspunkt;
+    private ArrayList<Pizza> ordreListe = new ArrayList<>;
+
+    // Constructor
+    public Ordre(int ordreId, int bestillingstidspunkt, int samletProduktionstid,
+                 int afhentningstidspunkt, ArrayList<Pizza> ordreListe;){
+        this.ordreId = ordreId;
+        this.bestillingstidspunkt = bestillingstidspunkt;
+        this.samletProduktionstid = samletProduktionstid;
+        this.afhentningstidspunkt = afhentningstidspunkt;
+        this.ordreListe = ordreListe;
+
+    }
 
 
 }
