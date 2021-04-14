42516288780dd6d83e0ba47bac49587621afa4b3
Lars <larsbrogaard86@gmail.com>
2021-04-14 10:19:23 +0200

    andet push

--- MarioPizzaBar/src/bestillingssystem/Bestillingsliste.java
+++ MarioPizzaBar/src/bestillingssystem/Bestillingsliste.java
@@ -2,23 +2,52 @@ package bestillingssystem;
 
 import java.util.ArrayList;
 
+import java.util.Collections;
+import java.util.Comparator;
+
+
 public class Bestillingsliste {
 
 
     // attributter
     private ArrayList<Ordre> bestillinger = new ArrayList<Ordre>();
-    private int ventetid = 10;
+    private int ventetid;
 
 
-    //
+    // constructor
+    public Bestillingsliste(ArrayList<Ordre> bestillinger, int ventetid){
+        this.bestillinger = bestillinger;
+        this.ventetid = ventetid;
+    }
 
 
     // metoder
+
+    public void tilføjOrdreTilBestillinger(Ordre ordreTilBestillinger){
+        bestillinger.add(ordreTilBestillinger);
+
+    }
+
     public int beregnAfhentningstidspunkt(int ventetid, int getsamletProduktionstid){
-        int afhentningstidspunkt = bestillingstidspunkt + ventetid + getsamletProduktionstid;
+        int afhentningstidspunkt = ventetid + getsamletProduktionstid;
         return afhentningstidspunkt;
     }
 
+    public void printBestillinger(){
+        System.out.println(bestillinger);
+    }
+
+
+    public static void main(String[] args) {
+        Ordre nyOdre = new Ordre();
+
+        System.out.println(nyOdre);
+
+        Bestillingsliste første = new Bestillingsliste(null,20);
+
+        første.printBestillinger();
+
+    }
 
 
 
