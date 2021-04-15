b6a0a3440e282ded09d0bf0b86dd096cf93a7304
KevinFHansen <kevi4777@stud.kea.dk>
2021-04-15 09:42:35 +0200

    Fixed UI Menu 15/4

--- MarioPizzaBar/src/UI_Menu/UImenu.java
+++ MarioPizzaBar/src/UI_Menu/UImenu.java
@@ -7,119 +7,103 @@ import java.util.Scanner;
 
 public class UImenu {
     public int scanUserInput;
-    Scanner scn = new Scanner(System.in);
+
     boolean exitProgram = false;
     Menukort menukort = new Menukort();
     Ordre ordre = new Ordre();
 
 
-    public void presentMenuOptions(){
-
-        System.out.println("Du har følgende muligheder :");
-        System.out.println("Tast 1 for at se Menu Kort");
-        System.out.println("Tast 2 for at håndtere ordrer");
-        System.out.println("Tast 3 for at håndtere menukortet");
-        System.out.println("Tast 4 for afslutte");
-        }
 
 
     public void startUI (){
+        printLogo();
         while (!exitProgram){
         presentMenuOptions();
+        Scanner scn = new Scanner(System.in);
         int userInput = scn.nextInt();
 
         switch (userInput) {
 
-        //Valg 1
+
+        //Valg 1 - Vis menukort
+
             case 1:
                 System.out.println("Her er menukortet");
                 menukort.visPizzaListe();
                 break;
 
-        //Valg 2
+        //Valg 2 - Håndter ordrer
             case 2:
-                System.out.println("Håndter ordrre");
-                System.out.println("Tast 1 for at se nuværende ordre");
-                System.out.println("Tast 2 for at oprette ny ordre");
-                System.out.println("Tast 3 for at slette ordre");
-                System.out.println("Tast 4 for at afslutte ordre");
-                System.out.println("Tast 5 for at vende tilbage til hovedmenu");
 
-                if (scn.nextInt() == 1)
+                subMenuOrdre();
+
+                int choice = scn.nextInt();
+
+                if (choice == 1) {
+                    System.out.println();
                     System.out.println("Her er din nuværende ordre");
                     //se din nuværende ordrer metode();
+                }
 
-                else if (scn.nextInt() == 2) {
+                else if (choice == 2) {
                     System.out.println("Du har valgt at oprette en ny ordre");
                     ordre.opretOrdre();
                 }
 
-                else if (scn.nextInt() == 3)
+                else if (choice== 3) {
+                    System.out.println();
                     System.out.println("Du har valgt at slette en ordre - Her er din ordre liste");
                     //slet ordre metode();
+                }
 
-                else if (scn.nextInt() == 4) {
+                else if (choice == 4) {
                     System.out.println("Du har valgt af afslutte ordren");
                     //afslut ordre metode();
+
+                }
+
+                else if (choice == 5) {
+                    System.out.println("Her er hovedmenuen");
+                    continue;
+                }
+
+                else {
+                    System.out.println("Forkert indtastning - Tast 1, 2, 3, 4 eller 5");
                     break;
                 }
 
-                else if (scn.nextInt() == 5)
-                    break;
-
-                else
-                    System.out.println("Forkert indtastning - Tast 1, 2, 3, 4 eller 5");
-
+            //Valg 3 - Skift Pizza
             case 3:
-                System.out.println("Hvilke ændringer vil du lave i menukortet?");
-                System.out.println("Tast 1 for at tilføje en pizza til menukortet");
-                System.out.println("Tast 2 for at fjerne en pizza fra menukortet");
-                System.out.println("Tast 3 for at ændre en pizza på menukortet");
-                System.out.println("Tast 4 for at vende tilbage til hovedmenu");
 
-                if (scn.nextInt() == 1) {
+               subMenuPizza();
+                int choice1 = scn.nextInt();
+
+                if (choice1 == 1) {
                     System.out.println("Du har valgt at tilføje en ny pizza til menukortet");
                     Menukort.opretNyPizza();
                 }
 
-                else if (scn.nextInt() == 2){
+                else if (choice1 == 2){
                     System.out.println("Du har valgt at fjerne en pizza fra menukortet");
                     menukort.fjernPizza();
                     }
 
-                else if (scn.nextInt() == 3){
+                else if (choice1 == 3){
                     System.out.println("Du har valgt at ændre en pizza på menukortet");
                     Menukort.skiftPizza();
                 }
-                else if (scn.nextInt() == 4)
+                else if (choice1 == 4) {
                     break;
+                }
 
-                else
+                else {
                     System.out.println("Forkert indtastning - Tast 1, 2, 3 eller 4");
 
-
-
-/*
-        //Valg 3
-            case 3:
-                System.out.println("Tilføj Pizza til menukortet");
-                Menukort.opretNyPizza();
+                }
                 break;
 
-        //Valg 4
-            case 4:
-                System.out.println("Fjern Pizza fra menukort");
-                menukort.fjernPizza();
-                break;
 
-        //Valg 5
-            case 5:
-                System.out.println("Ændre Pizza");
-                Menukort.skiftPizza();
-                break;*/
-
-
-        //Valg 6
+        //Valg 4 - Afslut ordre
             case 4:
                 System.out.println("Du har valgt at afslutte");
                 exitProgram = true;
@@ -129,8 +113,58 @@ public class UImenu {
         }
     }
 
+
+    public void presentMenuOptions(){
+        System.out.println();
+        System.out.println("Du har følgende valg:");
+        System.out.println("Tast 1 - Se Menukort");
+        System.out.println("Tast 2 - Håndter ordrer");
+        System.out.println("Tast 3 - Håndte Menukortet");
+        System.out.println("Tast 4 - Afslut");
+    }
+
+    public void subMenuOrdre (){
+        System.out.println("Håndter ordre - Du har følgende valg:");
+        System.out.println("Tast 1 - Se nuværerende ordre");
+        System.out.println("Tast 2 - Opret ordre");
+        System.out.println("Tast 3 - Slet ordre");
+        System.out.println("Tast 4 - Afslut ordre");
+        System.out.println("Tast 5 - Gå til Hovedmenu");
+    }
+    public void subMenuPizza (){
+        System.out.println("Håndter menukortet - Du har følgende valg:");
+        System.out.println("Tast 1 - Tilføj ny Pizza til Menukortet");
+        System.out.println("Tast 2 - Slet Pizza fra Menukortet");
+        System.out.println("Tast 3 - Ændre en Pizza på Menukortet");
+        System.out.println("Tast 4 for at vende tilbage til Hovedmenu");
+    }
+
+    public void printLogo (){
+
+        System.out.println("                        ___");
+        System.out.println("                        |%=@%%/");
+        System.out.println("                        |o%%%/");
+        System.out.println("                     __ |%%o/");
+        System.out.println("               _,--~~ | |(_/ ._");
+        System.out.println("            ,/'  m%%%%| |o/ /  `\\.");
+        System.out.println("           /' m%%o(_)%| |/ /o%%m `\\");
+        System.out.println("         /' %%@=%o%%%o|   /(_)o%%% `\\");
+        System.out.println("        /  %o%%%%%=@%%|  /%%o%%@=%%  \\");
+        System.out.println("       |  (_)%(_)%%o%%| /%%%=@(_)%%%  |");
+        System.out.println("       | %%o%%%%o%%%(_|/%o%%o%%%%o%%% |");
+        System.out.println("       | % ---> MARIO'S PIZZABAR <--- |");
+        System.out.println("       |  (_)%%=@%(_)%o%o%%(_)%o(_)%  |");
+        System.out.println("        \\ ~%%o%%%%%o%o%=@%%o%%@%%o%~ /");
+        System.out.println("         \\. ~o%%(_)%%%o%(_)%%(_)o~ ,/");
+        System.out.println("           \\_ ~o%=@%(_)%o%%(_)%~ _/");
+        System.out.println("             `\\_~~o%%%o%%%%%~~_/'");
+        System.out.println("                `--..____,,--'");
+    }
+
     public static void main(String[] args) {
         UImenu uImenu = new UImenu();
         uImenu.startUI();
+
     }
+
 }
